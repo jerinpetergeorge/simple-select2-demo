a9d503ed2f5be686c91b4d67a55d1e8b26bd81d7
Sebastian Pipping <sebastian@pipping.org>
2020-07-18 23:26:49 +0200

    forms: Make pub_date use the date picker native to the browser

--- demo/forms.py
+++ demo/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.forms import DateInput
 from simple_select2 import AutoCompleteSelect2Multiple, AutoCompleteSelect2
 from .models import Article
+
+
+class _NativeDatePickerWidget(DateInput):
+    input_type = 'date'
 
 
 class Select2ArticleModelForm(forms.ModelForm):
@@ -10,5 +15,5 @@ class Select2ArticleModelForm(forms.ModelForm):
         widgets = {
             'publications': AutoCompleteSelect2Multiple(url='select2-publication-list'),
             'reporter': AutoCompleteSelect2(url='select2-reporter-list'),
-            'pub_date': forms.SelectDateWidget
+            'pub_date': _NativeDatePickerWidget,
         }
